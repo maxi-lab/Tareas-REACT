435e67263e7fa4d86a8937847b9ecef4022931e2
Maximo <maxipreneste@gmail.com>
2024-01-03 21:36:55 -0300

    Listado de tareas

---
+++ src/tasks.js
@@ -0,0 +1,13 @@
+//simula una BBDD 
+export const tareas=[
+    {
+        id:1,
+        titulo:"tarea",
+        description:"Mi primer tarea"
+    },
+    {
+        id:2,
+        titulo:"messi",
+        description:"Mi primer messi"
+    },
+];
