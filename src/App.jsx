319ffc97402ae25e294d4ede865d3fc167999c05
Maximo <maxipreneste@gmail.com>
2024-01-06 12:52:37 -0300

    el fondo ahora ocupa toda la pantalla, ahora los id de las tereas ahora son secuenciales, dado que antes se pisaban

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { TaskList } from './TaskList'
 import { TaskForm } from './TaskForm';
-import { tareas } from "./tasks";
-import { useState, useEffect } from 'react';
-import { TaskContext } from './Context/TaskContext';
 export  function App() {
-  
   return (<>
             <main className='bg-zinc-900 h-screen'>
                   <div className='container mx-auto p-10'>
--- src/Context/TaskContext.jsx
+++ src/Context/TaskContext.jsx
@@ -2,9 +2,11 @@ import { createContext, useState, useEffect } from "react";
 export const TaskContext=createContext();
 export function TaskContextProvaider(props){
     const [tasks,setTasks]=useState([]);
+    const [idT,setIdT]=useState(0);
     function createTask(taskTitle, taskDescription) {
+      setIdT(idT+1);
       setTasks([...tasks,{
-        id:tasks.length+1,
+        id:idT,
         titulo:taskTitle,
         description:taskDescription
       }]);
