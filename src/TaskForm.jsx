424fa755f197f4aca8fb9ac07dbf297bd9bba519
Maximo <maxipreneste@gmail.com>
2024-01-06 15:32:10 -0300

    Animaciones

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { TaskContext } from "./Context/TaskContext";
+import { Fade } from "react-awesome-reveal";
 export function TaskForm(){
   
   const [title,setTitle]=useState("");
@@ -21,17 +22,16 @@ export function TaskForm(){
   
   return (<>
             <div className="max-w-md mx-auto ">
-            <form onSubmit={handeleSubmit} className="bg-slate-800 p-10 mb-4 rounded-lg">
+            <Fade cascade delay={500}><form onSubmit={handeleSubmit} className="bg-slate-800 p-10 mb-4 rounded-lg">
                 <h1 className="text-2xl font-bold text-white mb-3">Crea una tarea</h1>
                 <input placeholder="Escribe tu tarea" className="bg-slate-300 p-3 w-full mb-2 rounded-md" value={title} onChange={(e)=>setTitle(e.target.value)}/>
                 <textarea placeholder="Escribe la descripcion" className="bg-slate-300 p-3 w-full mb-2 rounded-md" value={description} onChange={(e)=>setDescription(e.target.value)}/>
-                <div className="p-2 text-red-300">{ valid ?'':'Se requiere un titulo y una descripcion'}</div>
+                <div className="p-2 text-red-300">{ valid ?'':<Fade direction="down">Se requiere un titulo y una descripcion</Fade>}</div>
                 <button className="bg-indigo-600 hover:bg-indigo-400 px-3 py-1 rounded-lg text-white">
                     Guardar
                 </button>
-            </form>   
-            
+            </form>
+            </Fade>
             </div>
-            
             </>);
 }
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,20 +1,21 @@
 import { DeleteTask } from "./DeleteTask";
 import { TaskContext } from "./Context/TaskContext";
 import { useContext } from "react";
+import { Fade } from "react-awesome-reveal";
 
 export function TaskList(){
     const {tasks}=useContext(TaskContext);
 
     if (tasks.length===0) {
-        return (<><div className="text-white font-bold text-center ">No hay tareas aun</div></>);
+        return (<><div className="text-white font-bold text-center "><Fade cascade duration={500} >No hay tareas aun</Fade></div></>);
     }
     return(
         <>
             <div className="grid grid-cols-3 gap-2 p-4 ">
-            {tasks.map((task)=><div key={task.id} className="bg-gray-800 text-white rounded-lg"><h1 className="text-xl font-bold p-2">Tarea: {task.titulo}</h1>
+            {tasks.map((task)=><Fade direction="up" triggerOnce  key={task.id} ><div  className="bg-gray-800 text-white rounded-lg"><h1 className="text-xl font-bold p-2">Tarea: {task.titulo}</h1>
             <h2 className="p-2">Descripcion: {task.description}</h2>
             <DeleteTask task={task}/>
-            </div>)}
+            </div></Fade>)}
             </div>
         </>
     );
