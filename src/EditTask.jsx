6ae313ec62a9fbbe316e6a5f1ad4edecd850ee94
Maximo <maxipreneste@gmail.com>
2024-01-07 01:09:27 -0300

    el boton de modificar estilizado

--- src/EditTask.jsx
+++ src/EditTask.jsx
@@ -3,6 +3,6 @@ import { TaskContext } from "./Context/TaskContext";
 export function EditTask({task}){
     const {modificar}=useContext(TaskContext);
     return(
-        <button onClick={()=>modificar(task.id)}>Modificar</button>
+        <div className="p-2"><button className="bg-blue-600 hover:bg-blue-400 rounded-lg mb-4 p-2 " onClick={()=>modificar(task.id)}>Modificar</button></div>
     )
 }
