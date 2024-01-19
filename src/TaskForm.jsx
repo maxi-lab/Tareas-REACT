9d4f23841473c13e0d42d8218bb35d8ce4496497
Maximo <maxipreneste@gmail.com>
2024-01-19 01:50:58 -0300

    carret modificado

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { TaskContext } from "./Context/TaskContext";
 import { Fade } from "react-awesome-reveal";
+
 export function TaskForm(){
   
   const [valid,setValid]=useState(true);
@@ -21,11 +22,11 @@ export function TaskForm(){
   
   return (<>
             <div className="max-w-md mx-auto ">
-            <Fade cascade delay={500} triggerOnce><form onSubmit={handeleSubmit} className="bg-slate-800 p-10 mb-4 rounded-lg">
+            <Fade cascade delay={500} bottom collapse when={valid}  triggerOnce><form onSubmit={handeleSubmit} className="bg-slate-800 p-10 mb-4 rounded-lg">
                 <h1 className="text-2xl font-bold text-white mb-3">Crea una tarea</h1>
-                <input placeholder="Escribe tu tarea" className="bg-slate-300 p-3 w-full mb-2 rounded-md" value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <textarea placeholder="Escribe la descripcion" className="bg-slate-300 p-3 w-full mb-2 rounded-md" value={description} onChange={(e)=>setDescription(e.target.value)}/>
-                <div className="p-2 text-red-300">{ valid ?'':<Fade direction="down">Se requiere un titulo y una descripcion para creacion de la tarea</Fade>}</div>
+                <input placeholder="Escribe tu tarea" className="bg-slate-300 p-3 w-full mb-2 rounded-md caret-slate-500"  value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                <textarea placeholder="Escribe la descripcion" className="bg-slate-300 p-3 w-full mb-2 rounded-md caret-slate-500" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+                <div className="p-2 text-red-300" style={{display:'block'}}>{ valid ?'':<Fade direction="down">Se requiere un titulo y una descripcion para creacion de la tarea</Fade>}</div>
                 <button className="bg-indigo-600 hover:bg-indigo-400 px-3 py-1 rounded-lg text-white">
                     Guardar
                 </button>
